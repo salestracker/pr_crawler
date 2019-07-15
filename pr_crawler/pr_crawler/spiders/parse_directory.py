a1841b988162de4b8151e16d590dc989f82d45d4
codecakes <akurnya@gmail.com>
2019-07-15 22:38:30 +0530

    parses fields successfully. need to clean data

--- pr_crawler/pr_crawler/spiders/parse_directory.py
+++ pr_crawler/pr_crawler/spiders/parse_directory.py
@@ -22,21 +22,17 @@ _DENY_LINKS = [
   r'company-profile/employees-biography/*',
 ]
 
-# XPATHS
-# _START_XPATH = ('//table/descendant::form/descendant::table/descendant
-#                 '::table/descendant::table/descendant::span/descendant::b
-#                 /a[@href]')
-
-_COMPANY_DESC_XPATH = ('//table[2]/descendant::table[contains(name(), '
-                       'tbody)]/descendant::div/div[2]/text()')
-_COMPANY_PROFILE_IMG = ('//table[2]/descendant::table/descendant::table[1]'
-                        '[contains(name(),tr[3])][contains(name(), '
-                        'h1)]//img/@src[starts-with(., "https")]|//table//tr['
-                        '2]//img/@src[starts-with(., "https")]')
-_COMPANY_NAME_XPATH = '//table[2]//table//table[1]//tr[3]//h1/text()'
-
-_COMPANY_FIELDS = {
-  'common': r'//table[3]//table',
+_COMPANY_FIELDS_COMMON = r'//table[3]//table'
+_COMPANY_FIELDS_1 = {
+  'name': '//table[2]//table//table[1]//tr[3]//h1/text()',
+  'company_description': ('//table[2]/descendant::table[contains(name(), '
+                          'tbody)]/descendant::div/div[2]/text()'),
+  'img': ('//table[2]/descendant::table/descendant::table[1]'
+          '[contains(name(),tr[3])][contains(name(), '
+          'h1)]//img/@src[starts-with(., "https")]|//table//tr['
+          '2]//img/@src[starts-with(., "https")]'),
+}
+_COMPANY_FIELDS_2 = {
   'status': r'//tr[1]/td[2]/text()',
   'founded': r'//tr[1]/td[4]/text()',
   'annual_revenue': r'//tr[1]/td[6]/text()',
@@ -46,6 +42,7 @@ _COMPANY_FIELDS = {
   'symbol': r'//tr[3]/td[2]/text()',
   'website': r'//tr[3]/td[4]/text()',
 }
+
 
 class ParseDirectorySpider(CrawlSpider):
   name = 'parse_directory'
@@ -69,13 +66,13 @@ class ParseDirectorySpider(CrawlSpider):
     :param response:
     :return:
     '''
+    
     # print(response.xpath(_START_XPATH))
     item = CompanyOverviewItem()
     item_loader = CompanyOverviewItemLoader(item=item, response=response)
-    item_loader.add_xpath('company_description', _COMPANY_DESC_XPATH)
-    item_loader.add_xpath('img', _COMPANY_PROFILE_IMG)
-    item_loader.add_xpath('name', _COMPANY_NAME_XPATH)
-    field_loader = item_loader.nested_xpath(_COMPANY_FIELDS['common'])
-    for key in _COMPANY_FIELDS:
-      field_loader.add_xpath(key, _COMPANY_FIELDS[key])
+    for key in _COMPANY_FIELDS_1:
+      item_loader.add_xpath(key, _COMPANY_FIELDS_1[key])
+    field_loader = item_loader.nested_xpath(_COMPANY_FIELDS_COMMON)
+    for key in _COMPANY_FIELDS_2:
+      field_loader.add_xpath(key, _COMPANY_FIELDS_2[key])
     return item_loader.load_item()
