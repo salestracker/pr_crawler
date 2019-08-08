fd67e6e92633ccc8d719996cf26669ba05583f52
codecakes <akurnya@gmail.com>
2019-08-09 02:16:14 +0530

    linted using yapf

--- pr_crawler/pr_crawler/spiders/parse_directory.py
+++ pr_crawler/pr_crawler/spiders/parse_directory.py
@@ -16,45 +16,46 @@ _BASE_URI = 'https://www.pr.com'
 _START_URI = r'https://www.pr.com/business-directory/'
 
 _BIZ_LINKS = [
-  r'business-directory/*',
+    r'business-directory/*',
 ]
 
 _COMPANY_LINK = [
-  r'company-profile/overview/[a-zA-Z0-9]+',
+    r'company-profile/overview/[a-zA-Z0-9]+',
 ]
 
 _DENY_LINKS = [
-  r'company-profile/industries/*',
-  r'company-profile/press-releases/*',
-  r'company-profile/contact-info/*',
-  r'company-profile/products-services/*',
-  r'company-profile/employees-list/*',
-  r'company-profile/employees/*',
-  r'company-profile/employees-biography/*',
-  r'company-profile/awards/*',
-  r'company-profile/investment-highlights/*',
+    r'company-profile/industries/*',
+    r'company-profile/press-releases/*',
+    r'company-profile/contact-info/*',
+    r'company-profile/products-services/*',
+    r'company-profile/employees-list/*',
+    r'company-profile/employees/*',
+    r'company-profile/employees-biography/*',
+    r'company-profile/awards/*',
+    r'company-profile/investment-highlights/*',
 ]
 
 _COMPANY_FIELDS_COMMON = r'//table[3]//table'
 _COMPANY_FIELDS_1 = {
-  'name': '//table[2]//table//table[1]//tr[3]//h1/text()',
-  'company_description': ('//table[2]/descendant::table[contains(name(), '
-                          'tbody)]/descendant::div/div[2]/text()'),
-  'img': ('//table[2]/descendant::table/descendant::table[1]'
-          '[contains(name(),tr[3])][contains(name(), '
-          'h1)]//img/@src[starts-with(., "https")]|//table//tr['
-          '2]//img/@src[starts-with(., "https")]'),
+    'name':
+        '//table[2]//table//table[1]//tr[3]//h1/text()',
+    'company_description': ('//table[2]/descendant::table[contains(name(), '
+                            'tbody)]/descendant::div/div[2]/text()'),
+    'img': ('//table[2]/descendant::table/descendant::table[1]'
+            '[contains(name(),tr[3])][contains(name(), '
+            'h1)]//img/@src[starts-with(., "https")]|//table//tr['
+            '2]//img/@src[starts-with(., "https")]'),
 }
 _COMPANY_FIELDS_2 = {
-  'status': r'//tr[1]/td[2]/text()',
-  'founded': r'//tr[1]/td[4]/text()',
-  'annual_revenue': r'//tr[1]/td[6]/text()',
-  'exchange': r'//tr[2]/td[2]/text()',
-  'pr_link_id': r'//tr[2]/td[4]/text()',
-  'employees': r'//tr[2]/td[6]/text()',
-  'symbol': r'//tr[3]/td[2]/text()',
-  'website': r'//tr[3]/td[4]/text()',
-  'url': '//table[2]//table//table[3]//tr/td//a[1]/@href[position()=1]',
+    'status': r'//tr[1]/td[2]/text()',
+    'founded': r'//tr[1]/td[4]/text()',
+    'annual_revenue': r'//tr[1]/td[6]/text()',
+    'exchange': r'//tr[2]/td[2]/text()',
+    'pr_link_id': r'//tr[2]/td[4]/text()',
+    'employees': r'//tr[2]/td[6]/text()',
+    'symbol': r'//tr[3]/td[2]/text()',
+    'website': r'//tr[3]/td[4]/text()',
+    'url': '//table[2]//table//table[3]//tr/td//a[1]/@href[position()=1]',
 }
 
 _COMPANY_CATEGORIES_URI = '//table[2]//table//table[3]//tr/td//i/a/@href'
@@ -71,11 +72,11 @@ _JOB_DIR = 'jobdir'
 _RAND_DIR = '{job_dir!s}_{rand_num:<3d}'
 # You can add many built in referenced settings here.
 _CUSTOM_SETTINGS = {
-  'JOBDIR': _RAND_DIR.format(
-    # Add as many keys as are preformatted and available in Custom Settings.
-    job_dir=_JOB_DIR,
-    rand_num=random.randrange(*_RAND_RANGE)
-  ).strip(),
+    'JOBDIR':
+        _RAND_DIR.format(
+            # Add as many keys as are preformatted and available in Custom Settings.
+            job_dir=_JOB_DIR,
+            rand_num=random.randrange(*_RAND_RANGE)).strip(),
 }
 
 
@@ -97,32 +98,29 @@ def preset_jobdir(custom_settings_dict):
   else:
     if not custom_settings_dict.get('JOBDIR', None):
       custom_settings_dict['JOBDIR'] = _RAND_DIR.format(
-        # Add as many keys as are preformatted and available in Custom Settings.
-        job_dir=_JOB_DIR,
-        rand_num=random.randrange(*_RAND_RANGE)
-      ).strip()
+          # Add as many keys as are preformatted and available in Custom Settings.
+          job_dir=_JOB_DIR,
+          rand_num=random.randrange(*_RAND_RANGE)).strip()
   return custom_settings_dict
 
 
 class ParseDirectorySpider(CrawlSpider):
   """Spider class for PR site."""
-  
+
   name = 'parse_directory'
   allowed_domains = ['pr.com']
   start_urls = [_START_URI]
   custom_settings = preset_jobdir(_CUSTOM_SETTINGS)
-  
+
   rules = (
-    Rule(
-      LinkExtractor(allow=_BIZ_LINKS, deny=_DENY_LINKS),
-      callback=None,
-      follow=True),
-    Rule(
-      LinkExtractor(allow=_COMPANY_LINK, deny=_DENY_LINKS + _BIZ_LINKS),
-      callback='parse_item',
-      follow=False),
+      Rule(LinkExtractor(allow=_BIZ_LINKS, deny=_DENY_LINKS),
+           callback=None,
+           follow=True),
+      Rule(LinkExtractor(allow=_COMPANY_LINK, deny=_DENY_LINKS + _BIZ_LINKS),
+           callback='parse_item',
+           follow=False),
   )
-  
+
   def parse_item(self, response):
     """Parse function for parsing company overview page of a company.
     
@@ -143,15 +141,16 @@ class ParseDirectorySpider(CrawlSpider):
     category_attributes = ['categories']
     contact_attributes = []
     href_dict = {
-      'endpoint': contact_href,
-      'parse_attributes': (contact_attributes, _CONTACT_PARSE_XPATH,
-                           'parse_contact'),
+        'endpoint':
+            contact_href,
+        'parse_attributes':
+            (contact_attributes, _CONTACT_PARSE_XPATH, 'parse_contact'),
     }
     uri = urlParse.urljoin(_BASE_URI, category_href)
     return self._yield_meta_request(uri, item_loader, _CATEGORIES_PARSE_XPATH,
-                                    category_attributes,
-                                    'parse_attribute', **href_dict)
-  
+                                    category_attributes, 'parse_attribute',
+                                    **href_dict)
+
   def parse_attribute(self, response):
     """Parse function for parsing category page of a company.
     
@@ -174,7 +173,7 @@ class ParseDirectorySpider(CrawlSpider):
     uri = urlParse.urljoin(_BASE_URI, href)
     return self._yield_meta_request(uri, item_loader, href_val[1], href_val[0],
                                     href_val[2])
-  
+
   def parse_contact(self, response):
     """Parse function for parsing contacts page.
     
@@ -201,10 +200,9 @@ class ParseDirectorySpider(CrawlSpider):
         else:
           contact_header = contact_header or 'Contact Info'
           contacts_list[contact_header].append(parsed_line)
-    
+
     for contact_header in contacts_list:
-      contact_loader = ContactItemLoader(
-        item=ContactItem(), response=response)
+      contact_loader = ContactItemLoader(item=ContactItem(), response=response)
       contact_array = contacts_list[contact_header]
       for idx in range(0, len(contact_array), 2):
         field = clean_field(contact_array[idx]).lower()
@@ -212,9 +210,9 @@ class ParseDirectorySpider(CrawlSpider):
       contacts_dict[contact_header].update(contact_loader.load_item())
     item_loader.add_value('contacts', contacts_dict)
     return item_loader.load_item()
-  
-  def _yield_meta_request(self, uri_endpoint, item_loader, xpath,
-                          item_key, callback_fn, **kwargs):
+
+  def _yield_meta_request(self, uri_endpoint, item_loader, xpath, item_key,
+                          callback_fn, **kwargs):
     """Yields a request object based on meta object.
     
     params:
@@ -230,9 +228,9 @@ class ParseDirectorySpider(CrawlSpider):
     """
     uri = urlParse.urljoin(_BASE_URI, uri_endpoint)
     meta = {
-      'item_dict': item_loader.load_item(),
-      'attributes': item_key,
-      'parse_xpath': xpath,
+        'item_dict': item_loader.load_item(),
+        'attributes': item_key,
+        'parse_xpath': xpath,
     }
     if kwargs:
       meta.update(kwargs)
