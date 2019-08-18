bd7a9501c20c5b8a5068182b6e26b0df29a5df7c
codecakes <akurnya@gmail.com>
2019-08-18 22:06:41 +0530

    (codecakes): Fix to bypass GOOGLE CREDENTIALS GOOGLE_APPLICATION_CREDENTIALS

--- pr_crawler/pr_crawler/__init__.py
+++ pr_crawler/pr_crawler/__init__.py
@@ -4,11 +4,25 @@ transaction = db.transaction()
 # For batch writes.
 batch = db.batch()
 '''
-
+import os
+from google.oauth2 import service_account
 from google.cloud import firestore
 
 # Project ID is determined by the GCLOUD_PROJECT environment variable
-FIRE_DB = firestore.Client()
+_PROJECT = 'supplytracker'
+_KEY_FILE = 'supplytracker-3b70d75569c5.json'
+_CREDENTIALS = None
+
+cur_path = os.path.realpath(os.path.abspath(__file__))
+parent_path = os.path.split(os.path.split(cur_path)[0])[0]
+key_file = os.path.join(parent_path, _KEY_FILE)
+
+if os.path.exists(key_file) and os.path.isfile(key_file):
+  _CREDENTIALS = service_account.Credentials.from_service_account_file(key_file)
+else:
+  raise NotImplementedError('service key file not found')
+
+FIRE_DB = firestore.Client(project=_PROJECT, credentials=_CREDENTIALS)
 
 # Parsing urls
 _BASE_URI = 'https://www.pr.com'
